64a8cb8916fa4fa2591db333a995c3110b828cd0
Filipe Bezerra <filipebzerra@gmail.com>
2013-02-11 01:21:47 -0200

    Added unit test for Database Connection factory.

---
+++ src/br/ucg/cmp1017/jweatherpanel/model/dao/test/UTestDatabaseConnection.java
@@ -0,0 +1,14 @@
+package br.ucg.cmp1017.jweatherpanel.model.dao.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UTestDatabaseConnection {
+
+	@Test
+	public void testGetConnection() {
+		fail("Not yet implemented");
+	}
+
+}
